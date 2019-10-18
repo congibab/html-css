c823f9c175aa11f7f5fda0aa7c613abc01f3c2bd
congibab <49512074+congibab@users.noreply.github.com>
2019-10-18 17:01:19 +0900

    191018
    ue4 unitychan movement

--- Web/Main.js
+++ Web/Main.js
@@ -1,5 +1,10 @@
 function showClock1() {
     var now = new Date();
-   document.getElementById("Time").innerHTML = now;
+    var nowHour = now.getHours();
+    var nowMin = now.getMinutes();
+    var nowSec = now.getSeconds();
+    var msg = nowHour + ":" + nowMin + ":" + nowSec; 
+
+   document.getElementById("Time").innerHTML = msg;
 }
 setInterval('showClock1()', 1000);
